ffb5c26554a94ad883fabfd5eedd551fbee68b7c
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-04-26 20:52:22 -0500

    first commit arquetipo

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <>
+         <section className="home-container">
+            <article>
+                <h2> ¿Que es lo que hacemos? </h2>
+            </article>
+        </section >
+        </>
+       
+    )
+}
+
+export default Home
---
+++ frontend/src/pages/Tareas.jsx
@@ -0,0 +1,8 @@
+function Tareas() {
+    return(
+        <>
+            
+        </>
+    )
+}
+export default Tareas
