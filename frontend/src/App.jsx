dde23c78ee79710a555150648ca74318940fa978
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-04-28 22:53:08 -0500

    añadida ruta a la pagina de registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home'
 import Nav from './components/Nav/Nav'
 import Tareas from './pages/Tareas/Tareas'
 import CrearTarea from './pages/CrearTarea/CrearTarea'
+import Registro from './pages/Registro/Registro'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Route path='/' element={<Home />}/>
                     <Route path='/tareas' element={<Tareas />} />
                     <Route path='/crear-tarea' element={<CrearTarea />} />
+                    <Route path='/registro' element={<Registro />} />
                 </Routes>
             </BrowserRouter>
         </>
