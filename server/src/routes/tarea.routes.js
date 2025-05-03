bc59da75b1c8608395c275b5469a87196631c5bd
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-03 18:07:34 -0500

    añadidas ruta para obtener todas las tareas

--- server/src/routes/tarea.routes.js
+++ server/src/routes/tarea.routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 import tareaController from '../controllers/tarea.controller.js'
 import { tareaSchema } from '../dtos/tarea.dto.js'
 import { validate } from '../middlewares/validate.middleware.js'
+import tarea from '../models/tarea.model.js';
 
-router.post('/', validate(tareaSchema), tareaController.crearTarea);
+router.post('/', validate(tareaSchema), tareaController.crearTarea)
+router.get('/', validate(tareaSchema), tareaController.getAllTareas)
 
 export default router
