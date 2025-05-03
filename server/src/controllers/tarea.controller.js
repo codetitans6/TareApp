59fb863e3a222cb23a83621cbdc310c418e930fc
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-03 18:06:12 -0500

    añadida funcion de obtener todas las tareas

--- server/src/controllers/tarea.controller.js
+++ server/src/controllers/tarea.controller.js
@@ -4,11 +4,21 @@ const crearTarea = async (req, res) => {
   try {
     const tarea = await tareaService.crearTarea(req.body);
     res.status(201).json(tarea);
-  } catch (err) {
+  } catch (error) {
+    console.error(error)
     res.status(500).json({ error: 'Error al crear tarea' });
   }
 };
-
+const getAllTareas = async (req, res) => {
+  try {
+    const tareas = await tareaService.getAllTareas()
+    res.status(200).json(tareas)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error al obtener tareas' });
+  }
+}
 export default {
-  crearTarea
+  crearTarea,
+  getAllTareas
 }
