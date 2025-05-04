1c5ad41a7368ffdc75ecc2703ed5a63d61ad815d
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-03 22:42:37 -0500

    añadidas rutas para obtener tareas por id y actualizar tarea

--- server/src/routes/tarea.routes.js
+++ server/src/routes/tarea.routes.js
@@ -1,11 +1,12 @@
-import express from 'express'
+import express, { Router } from 'express'
 const router = express.Router();
 import tareaController from '../controllers/tarea.controller.js'
 import { tareaSchema } from '../dtos/tarea.dto.js'
 import { validate } from '../middlewares/validate.middleware.js'
-import tarea from '../models/tarea.model.js';
 
 router.post('/', validate(tareaSchema), tareaController.crearTarea)
 router.get('/', validate(tareaSchema), tareaController.getAllTareas)
+router.get('/:id', validate(tareaSchema), tareaController.getTareaById)
+router.put('/:id', validate(tareaSchema), tareaController.updateTarea)
 
 export default router
