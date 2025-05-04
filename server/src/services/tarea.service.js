1fa560e9fb588cbc7dde79902d1ed25305eef87c
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-03 22:41:38 -0500

    añadidos enpoints para obtener y actualizar tarea por su id

--- server/src/services/tarea.service.js
+++ server/src/services/tarea.service.js
@@ -6,10 +6,20 @@ const crearTarea = async (data) => {
 };
 
 const getAllTareas = async () => {
-  const tareas = Tarea.find()
-  return await tareas
+  return await Tarea.find()
 }
+const getTareaById = async (id) => {
+  return Tarea.findById(id)
+}
+
+const updateTarea = async (id, data) => {
+  const updatedTarea = await Tarea.findByIdAndUpdate(id, data, { new: true });
+  return updatedTarea;
+};
+
 export default {
   crearTarea,
-  getAllTareas
+  getAllTareas,
+  getTareaById,
+  updateTarea
 };
