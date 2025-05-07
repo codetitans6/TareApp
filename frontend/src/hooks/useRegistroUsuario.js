92b20b1f59d09a7d87c968db4168e2529ebc24eb
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-07 16:46:59 -0500

    modificado registro de usuarios para añadir token de autenticacion

--- frontend/src/hooks/useRegistroUsuario.js
+++ frontend/src/hooks/useRegistroUsuario.js
@@ -1,18 +1,21 @@
 import { useState } from 'react'
 import { crearUsuario } from '../services/usuarioService'
-
+import { useAuth } from '../context/AuthContext';
 
 const useRegistroUsuario = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(false);
+    const { setToken } = useAuth();
 
     const submitUsuarioNuevo = async (usuarioData, callback) => {
         setLoading(true)
         setError(null)
         setSuccess(false)
         try {
-            await crearUsuario(usuarioData)
+            const data = await crearUsuario(usuarioData);
+            if (data.token) setToken(data.token);
+            if (data.usuario) localStorage.setItem('id', data.usuario.id)
             setSuccess(true)
             if (callback) callback()
         } catch (error) {
