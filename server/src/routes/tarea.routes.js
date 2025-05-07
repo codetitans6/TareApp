06f066d8443d9101f12cae8eb187b3edc2b74358
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-07 16:53:42 -0500

    añadidas nuevas rutas de asignar tarea

--- server/src/routes/tarea.routes.js
+++ server/src/routes/tarea.routes.js
@@ -4,9 +4,11 @@ import tareaController from '../controllers/tarea.controller.js'
 import { tareaSchema } from '../dtos/tarea.dto.js'
 import { validate } from '../middlewares/validate.middleware.js'
 
-router.post('/', validate(tareaSchema), tareaController.crearTarea)
-router.get('/', validate(tareaSchema), tareaController.getAllTareas)
-router.get('/:id', validate(tareaSchema), tareaController.getTareaById)
-router.put('/:id', validate(tareaSchema), tareaController.updateTarea)
-
+router.post('/', validate(tareaSchema), tareaController.crearTarea);
+router.get('/tareas/:id', tareaController.getAllUserTareas);
+router.get('/:id', tareaController.getTareaById); 
+router.put('/:id', validate(tareaSchema), tareaController.updateTarea);
+router.put('/asignar', tareaController.addUsuariosTarea);
+router.get('/usuario/:correo', tareaController.getUsuarioId); 
+router.get('/usuarios/:tareaId', tareaController.getUsuariosInTarea)
 export default router
