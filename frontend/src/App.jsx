b296303e7568ab0b665aa07533c2f747a1939f91
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-10 17:45:27 -0500

    añadida ruta de inicio de sesion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Tareas from './pages/Tareas/Tareas'
 import CrearTarea from './pages/CrearTarea/CrearTarea'
 import Registro from './pages/Registro/Registro'
 import EditarTarea from './pages/EditarTarea/EditarTarea'
+import InicioSesion from './pages/InicioSesion/InicioSesion'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
     return (
@@ -12,10 +13,11 @@ function App() {
             <BrowserRouter>
                 <Nav />
                 <Routes>
-                    <Route path='/' element={<Home />}/>
+                    <Route path='/' element={<Home />} />
                     <Route path='/tareas' element={<Tareas />} />
                     <Route path='/crear-tarea' element={<CrearTarea />} />
                     <Route path='/registro' element={<Registro />} />
+                    <Route path='/inicio-sesion' element={<InicioSesion />} />
                     <Route path='/editar/:id' element={<EditarTarea />} />
                 </Routes>
             </BrowserRouter>
