c0def5623980d6e0a97ce5704d8156e8d17492f0
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-12 14:12:30 -0500

    añadida logica para guardar datos de usuario

--- frontend/src/hooks/useInicioSesion.js
+++ frontend/src/hooks/useInicioSesion.js
@@ -6,28 +6,29 @@ const useInicioSesion = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(false);
-    const { setToken } = useAuth();
+    const { setToken, setUsuario } = useAuth()
 
-   const submitInicioSesion = async ({ correo, contrasena }, callback) => {
-    setLoading(true);
-    setError(null);
-    setSuccess(false);
-    try {
-        const data = await inicioSesion(correo, contrasena);
-        if (data.error) {
-            throw new Error(data.error);
+
+    const submitInicioSesion = async ({ correo, contrasena }, callback) => {
+        setLoading(true);
+        setError(null);
+        setSuccess(false);
+        try {
+            const data = await inicioSesion(correo, contrasena);
+            if (data.error) {
+                throw new Error(data.error);
+            }
+            if (data.token) setToken(data.token);
+            if (data.usuario) localStorage.setItem('id', data.usuario.id);
+            if (data.usuario) setUsuario(data.usuario);
+            setSuccess(true);
+            if (callback) callback();
+        } catch (error) {
+            setError(error.message || 'Error inesperado');
+        } finally {
+            setLoading(false);
         }
-        if (data.token) setToken(data.token);
-        if (data.usuario) localStorage.setItem('id', data.usuario.id);
-
-        setSuccess(true);
-        if (callback) callback();
-    } catch (error) {
-        setError(error.message || 'Error inesperado');
-    } finally {
-        setLoading(false);
-    }
-};
+    };
 
     return { submitInicioSesion, loading, error, success };
 }
