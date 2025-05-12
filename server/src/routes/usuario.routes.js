b6224db40d01186871716331999377227665d7b6
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-12 14:16:08 -0500

    añadida ruta para eliminar usuario

--- server/src/routes/usuario.routes.js
+++ server/src/routes/usuario.routes.js
@@ -7,5 +7,5 @@ import { validate } from '../middlewares/validate.middleware.js'
 
 router.post('/', validate(usuarioSchema), usuarioController.registro)
 router.post('/inicio', usuarioController.inicioSesion)
-
+router.post('/eliminar/:userId', usuarioController.eliminarCuenta);
 export default router
