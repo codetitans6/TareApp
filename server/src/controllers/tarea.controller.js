7a13db3b5d86c6f50c195b838c52c6604c3f8a67
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-13 21:14:43 -0500

    modificado enpoint de actualizar tarea

--- server/src/controllers/tarea.controller.js
+++ server/src/controllers/tarea.controller.js
@@ -1,5 +1,7 @@
 import tareaService from '../services/tarea.service.js'
-
+import Tarea from '../models/tarea.model.js';
+import Notificacion from '../models/notificacion.model.js';
+import Usuario from '../models/usuario.model.js'
 const crearTarea = async (req, res) => {
   try {
     const tarea = await tareaService.crearTarea(req.body);
@@ -33,17 +35,40 @@ const getTareaById = async (req, res) => {
   }
 };
 
+
+
 const updateTarea = async (req, res) => {
-  const { id } = req.params
-  const data = req.body
   try {
-    const updatedTarea = await tareaService.updateTarea(id, data)
-    res.status(200).json(updatedTarea)
+    const { id } = req.params;
+    const nuevaData = req.body;
+
+    const tareaActual = await Tarea.findById(id);
+    if (!tareaActual) {
+      return res.status(404).json({ error: 'Tarea no encontrada' });
+    }
+
+    const anteriores = new Set(tareaActual.usuarios.map(u => u.toString()));
+    const nuevos = (nuevaData.usuarios || []).filter(u => !anteriores.has(u));
+    const creador = await Usuario.findById(tareaActual.creador).select("nombre");
+    const tareaActualizada = await tareaService.updateTarea(id, nuevaData);
+
+    if (nuevos.length > 0) {
+      const notificaciones = nuevos.map(userId => ({
+        usuario: userId,
+        mensaje: `${creador.nombre} te ha asignado la tarea: ${tareaActualizada.titulo}`,
+        tarea: tareaActualizada._id,
+      }));
+      await Notificacion.insertMany(notificaciones);
+    }
+
+    res.status(200).json(tareaActualizada);
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Error al actualizar la tarea' })
+    console.error('Error en updateTarea:', error);
+    res.status(500).json({ error: 'Error al actualizar la tarea' });
   }
-}
+};
+
+
 const getUsuarioId = async (req, res) => {
   const { correo } = req.params
   try {
@@ -87,5 +112,4 @@ export default {
   getUsuarioId,
   getUsuariosInTarea,
   completarTarea
-
 }
